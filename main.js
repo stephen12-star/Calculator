967aa29fe5540e7b0beb179f19cde4dea1babe25
stephen12-star <stephen.crenshaw23@student.cart.org>
2024-01-25 10:04:33 -0800

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+const display1E1 = document.querySelector ('.display-1');
+const display2E1 = document.querySelector ('.display-2');
+const tempResult1E1 = document.querySelector ('.temp-reslut');
+const numbersE1 = document.querySelectorAll ('.number');
+const operationE1 = document.querySelectorAll ('.operation');
+const equalE1 = document.querySelectorAll ('.equal');
+const numbers = document.querySelectorAll ('.equal');
