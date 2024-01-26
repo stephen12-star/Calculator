3ce9adc7493e85ffb1eb52bba1650684a692cc29
stephen12-star <143832141+stephen12-star@users.noreply.github.com>
2024-01-26 08:36:00 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,22 @@ const display2E1 = document.querySelector ('.display-2');
 const tempResult1E1 = document.querySelector ('.temp-reslut');
 const numbersE1 = document.querySelectorAll ('.number');
 const operationE1 = document.querySelectorAll ('.operation');
-const equalE1 = document.querySelectorAll ('.equal');
-const numbers = document.querySelectorAll ('.equal');
+const equalE1 = document.querySelector ('.equal');
+const clearE1 = document.querySelector ('.all-clear');
+const clearLastE1 = document.querySelector ('.last-entity-clear');
+
+let dis1Num ='';
+let dis2Num ='';
+let reult = null;
+let lastOperation= '';
+let haveDot = false;
+
+numbersE1.forEach (number => {
+    number.addEventListener ('click', (e)=> {
+        if (e.target.innerText === '.' && !haveDot){
+            haveDot=true;
+        } else if (e.target.innerText=== '.' && haveDot){
+            return;
+        }
+    })
+})
